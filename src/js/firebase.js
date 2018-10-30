870208810d3df9f0a573be0f373c8ea6691f5c26
spacedgatito <miluska.gonzalezar@gmail.com>
2018-10-30 11:06:11 -0500

    Agrega archivos iniciales

---
+++ src/js/firebase.js
@@ -0,0 +1,11 @@
+ // Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyDsfk0M5QKKKUYOJ4mqAzK_KB6ICwCXWeA",
+   authDomain: "americatv-ads.firebaseapp.com",
+   databaseURL: "https://americatv-ads.firebaseio.com",
+   projectId: "americatv-ads",
+   storageBucket: "americatv-ads.appspot.com",
+   messagingSenderId: "507467215527"
+ };
+ 
+ firebase.initializeApp(config);
