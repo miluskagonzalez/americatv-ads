e8f540bef9a175767cfecbc600833a8d067a5c0f
spacedgatito <miluska.gonzalezar@gmail.com>
2018-10-31 12:39:31 -0500

    Agrega redirección por autenticación

--- src/js/login.js
+++ src/js/login.js
@@ -3,5 +3,6 @@ const loginForm = document.getElementById('login');
 loginForm.addEventListener('submit', event => {
   event.preventDefault();
   const { target: { email, password } } = event;
-  signIn(email.value, password.value).then(console.log).catch(console.error)
+  signIn(email.value, password.value)
+    .then(() => window.location = './home.html')
 })
