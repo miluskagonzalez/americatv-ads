02962913548662c234bca4fc1e6d89c3c037969a
spacedgatito <miluska.gonzalezar@gmail.com>
2018-10-30 23:29:36 -0500

    Configura firebase, funcionalidad login/signup, guarda datos de usuario

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,11 +1,20 @@
- // Initialize Firebase
- var config = {
-   apiKey: "AIzaSyDsfk0M5QKKKUYOJ4mqAzK_KB6ICwCXWeA",
-   authDomain: "americatv-ads.firebaseapp.com",
-   databaseURL: "https://americatv-ads.firebaseio.com",
-   projectId: "americatv-ads",
-   storageBucket: "americatv-ads.appspot.com",
-   messagingSenderId: "507467215527"
- };
- 
- firebase.initializeApp(config);
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyDsfk0M5QKKKUYOJ4mqAzK_KB6ICwCXWeA",
+  authDomain: "americatv-ads.firebaseapp.com",
+  databaseURL: "https://americatv-ads.firebaseio.com",
+  projectId: "americatv-ads",
+  storageBucket: "americatv-ads.appspot.com",
+  messagingSenderId: "507467215527"
+});
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+db.settings({ timestampsInSnapshots: true });
+
+//función registro
+const signUp = (email, password) => auth.createUserWithEmailAndPassword(email, password);
+// función inicio de sesión
+const signIn = (email, password) => auth.signInWithEmailAndPassword(email, password);
+
+const saveUser = (user) => db.doc(`users/${user.uid}`).set(user);
